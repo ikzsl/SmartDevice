309cf762866d18c173133fb23368a07b49edfe8a
dmm <ikzsl@yandex.ru>
2020-03-16 20:04:05 +0300

    Добавляет проверку существования элемента

--- source/js/main.js
+++ source/js/main.js
@@ -4,15 +4,23 @@ var modalCloseButton = document.querySelector('.modal-feedback__close');
 var callbackButton = document.querySelector('.button--callback');
 var popupLayer = document.querySelector('.popup-layer');
 
-function onCallbackButton() {
+function onCallbackButtonClick() {
   modalWindow.classList.remove('modal-feedback--off');
   popupLayer.classList.remove('popup-layer--off');
 }
-callbackButton.addEventListener('click', onCallbackButton);
+
+if (callbackButton) {
+  callbackButton.addEventListener('click', onCallbackButtonClick);
+}
 
 
-function onModalCloseButton() {
+
+function onModalCloseButtonClick() {
   modalWindow.classList.add('modal-feedback--off');
   popupLayer.classList.add('popup-layer--off');
 }
-modalCloseButton.addEventListener('click', onModalCloseButton);
+
+if (modalCloseButton) {
+  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
+}
+
