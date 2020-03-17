6f1c65394928577e348346e5e81c984566fcab3a
dmm <ikzsl@yandex.ru>
2020-03-18 02:40:07 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 'use strict';
+var imask = window.IMask;
 
-import IMask from 'imask.js';
+imask(document.getElementById('modal-tel'), {
+  mask: '+{7}(000)000-00-00'
+});
 
-console.log(imask);
+imask(document.getElementById('form-tel'), {
+  mask: '+{7}(000)000-00-00'
+});
+
 
 var modalWindow = document.querySelector('.modal-feedback');
 var modalCloseButton = document.querySelector('.modal-feedback__close');
 var callbackButton = document.querySelector('.button--callback');
 var popupLayer = document.querySelector('.popup-layer');
 
-function onCallbackButtonClick() {
+var showPopup = function () {
   modalWindow.classList.remove('modal-feedback--off');
   popupLayer.classList.remove('popup-layer--off');
+};
+
+var hidePopup = function () {
+  modalWindow.classList.add('modal-feedback--off');
+  popupLayer.classList.add('popup-layer--off');
+};
+
+var onPopupLayerClick = function () {
+  hidePopup();
+  popupLayer.removeEventListener('click', onPopupLayerClick);
+};
+
+var onEscKeyDown = function (evt) {
+  if (evt.keyCode === 27) {
+    onPopupLayerClick();
+  }
+};
+
+var onCallbackButtonClick = function () {
+  showPopup();
+  document.addEventListener('keydown', onEscKeyDown);
+  popupLayer.addEventListener('click', onPopupLayerClick);
+};
+
+function onModalCloseButtonClick() {
+  hidePopup();
+  popupLayer.removeEventListener('click', onPopupLayerClick);
 }
 
 if (callbackButton) {
   callbackButton.addEventListener('click', onCallbackButtonClick);
 }
 
-
-
-function onModalCloseButtonClick() {
-  modalWindow.classList.add('modal-feedback--off');
-  popupLayer.classList.add('popup-layer--off');
-}
-
 if (modalCloseButton) {
   modalCloseButton.addEventListener('click', onModalCloseButtonClick);
 }
-
