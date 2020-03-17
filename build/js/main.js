1e4bf2a09db6b973d3f43807e38a53e0d30b9c61
dmm <ikzsl@yandex.ru>
2020-03-17 20:47:47 +0300

    Добавляет обработчики попапа

--- build/js/main.js
+++ build/js/main.js
@@ -17,9 +17,32 @@ var modalCloseButton = document.querySelector('.modal-feedback__close');
 var callbackButton = document.querySelector('.button--callback');
 var popupLayer = document.querySelector('.popup-layer');
 
-function onCallbackButtonClick() {
+var showPopup = function () {
   modalWindow.classList.remove('modal-feedback--off');
   popupLayer.classList.remove('popup-layer--off');
+};
+
+var hidePopup = function () {
+  modalWindow.classList.add('modal-feedback--off');
+  popupLayer.classList.add('popup-layer--off');
+};
+
+var onPopupLayerClick = function () {
+  hidePopup();
+  popupLayer.removeEventListener('click', onPopupLayerClick);
+}
+
+
+var onCallbackButtonClick = function () {
+  showPopup();
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      hidePopup();
+      popupLayer.removeEventListener('click', onPopupLayerClick);
+    }
+  });
+
+  popupLayer.addEventListener('click', onPopupLayerClick);
 }
 
 if (callbackButton) {
@@ -27,13 +50,13 @@ if (callbackButton) {
 }
 
 
-
 function onModalCloseButtonClick() {
-  modalWindow.classList.add('modal-feedback--off');
-  popupLayer.classList.add('popup-layer--off');
+  hidePopup()
+  popupLayer.removeEventListener('click', onPopupLayerClick);
 }
 
 if (modalCloseButton) {
   modalCloseButton.addEventListener('click', onModalCloseButtonClick);
 }
+
 
--- source/js/main.js
+++ source/js/main.js
@@ -11,26 +11,46 @@ var formPhoneMask = IMask(
 });
 
 
-
 var modalWindow = document.querySelector('.modal-feedback');
 var modalCloseButton = document.querySelector('.modal-feedback__close');
 var callbackButton = document.querySelector('.button--callback');
 var popupLayer = document.querySelector('.popup-layer');
 
-function onCallbackButtonClick() {
+var showPopup = function () {
   modalWindow.classList.remove('modal-feedback--off');
   popupLayer.classList.remove('popup-layer--off');
+};
+
+var hidePopup = function () {
+  modalWindow.classList.add('modal-feedback--off');
+  popupLayer.classList.add('popup-layer--off');
+};
+
+var onPopupLayerClick = function () {
+  hidePopup();
+  popupLayer.removeEventListener('click', onPopupLayerClick);
+}
+
+
+var onCallbackButtonClick = function () {
+  showPopup();
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      hidePopup();
+      popupLayer.removeEventListener('click', onPopupLayerClick);
+    }
+  });
+
+  popupLayer.addEventListener('click', onPopupLayerClick);
 }
 
 if (callbackButton) {
   callbackButton.addEventListener('click', onCallbackButtonClick);
 }
 
-
-
 function onModalCloseButtonClick() {
-  modalWindow.classList.add('modal-feedback--off');
-  popupLayer.classList.add('popup-layer--off');
+  hidePopup()
+  popupLayer.removeEventListener('click', onPopupLayerClick);
 }
 
 if (modalCloseButton) {
